601913e805ed5fcb997439bcaf2dbdd46ebc41b3
RRYYOOTTAA3110 <john0316shinkou@gmail.com>
2019-05-08 08:54:39 +0900

    Repair1
    商品詳細ページの不具合を修正しました。

--- WebContent/WEB-INF/jsp/PE0005.jsp
+++ WebContent/WEB-INF/jsp/PE0005.jsp
@@ -1,7 +1,32 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-pageEncoding="UTF-8" %>
-<jsp:include page="/WEB-INF/jsp/template.jsp">
-<jsp:param name="id" value="PE0005" />
-<jsp:param name="name" value="プリンタ" />
-<jsp:param name="price" value="39800" />
-</jsp:include>
+pageEncoding="UTF-8"
+import="jp.webschool.javaweb.chapter17.util.PropertyLoader"
+import="jp.webschool.javaweb.chapter17.util.Encoder"%>
+<% request.setCharacterEncoding("utf-8"); %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>プリンタ</title>
+<link href="css/table.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<h1>プリンタ</h1>
+<table>
+<tr>
+<th id="id">ID</th>
+<th>商品名</th>
+<th id="price">価格</th>
+</tr>
+<tr>
+<td>PE0005</td>
+<td>プリンタ</td>
+<td>39800円</td>
+</tr>
+<tr>
+<td colspan="3"><a href="<%=PropertyLoader.getProperty("url.servlet.ItemManager") %>">一覧へ戻る</a></td>
+</tr>
+</table>
+</body>
+</html>
+
--- src/jp/webschool/javaweb/chapter17/html/CreateContent.java
+++ src/jp/webschool/javaweb/chapter17/html/CreateContent.java
@@ -23,13 +23,44 @@ public class CreateContent {
 				PrintWriter pw = new PrintWriter
 						(new BufferedWriter(new FileWriter("C:/java/w17manipulation_test2/WebContent/WEB-INF/jsp/" + id + ".jsp")));
 
-				pw.println("<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\"\n"
+				/*pw.println("<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\"\n"
 						+ "pageEncoding=\"UTF-8\" %>\n"
 						+ "<jsp:include page=\"" + productPage + "template.jsp\">\n"
 						+ "<jsp:param name=\"id\" value=\"" + id + "\" />\n"
 						+ "<jsp:param name=\"name\" value=\"" + name + "\" />\n"
 						+ "<jsp:param name=\"price\" value=\"" + price + "\" />\n"
-						+ "</jsp:include>");
+						+ "</jsp:include>");*/
+				pw.println("<%@ page language=\"java\" contentType=\"text/html; charset=UTF-8\"\n"
+						+ "pageEncoding=\"UTF-8\"\n"
+						+ "import=\"jp.webschool.javaweb.chapter17.util.PropertyLoader\"\n"
+						+ "import=\"jp.webschool.javaweb.chapter17.util.Encoder\"%>\n"
+						+ "<% request.setCharacterEncoding(\"utf-8\"); %>\n"
+						+ "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">\n"
+						+ "<html>\n"
+						+ "<head>\n"
+						+ "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n"
+						+ "<title>" + name + "</title>\n"
+						+ "<link href=\"css/table.css\" rel=\"stylesheet\" type=\"text/css\">\n"
+						+ "</head>\n"
+						+ "<body>\n"
+						+ "<h1>" + name + "</h1>\n"
+						+ "<table>\n"
+						+ "<tr>\n"
+						+ "<th id=\"id\">ID</th>\n"
+						+ "<th>商品名</th>\n"
+						+ "<th id=\"price\">価格</th>\n"
+						+ "</tr>\n"
+						+ "<tr>\n"
+						+ "<td>" + id + "</td>\n"
+						+ "<td>" + name + "</td>\n"
+						+ "<td>" + price + "円</td>\n"
+						+ "</tr>\n"
+						+ "<tr>\n"
+						+ "<td colspan=\"3\"><a href=\"<%=PropertyLoader.getProperty(\"url.servlet.ItemManager\") %>\">一覧へ戻る</a></td>\n"
+						+ "</tr>\n"
+						+ "</table>\n"
+						+ "</body>\n"
+						+ "</html>\n");
 				pw.close();
 			}
 
